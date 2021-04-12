ac9dc2605a93e9b369e19fc9576dafd60e58d623
t-ober <63147366+t-ober@users.noreply.github.com>
2021-04-12 11:46:06 +0200

    added json grid parser

---
+++ src/main/scala/edu/ie3/powerFactory2psdm/io/GridParser.scala
@@ -0,0 +1,30 @@
+package edu.ie3.powerFactory2psdm.io
+
+import com.typesafe.scalalogging.LazyLogging
+import edu.ie3.powerFactory2psdm.model.powerfactory.PowerFactoryGrid
+import io.circe.parser.decode
+import io.circe.generic.auto._
+import io.circe.parser._
+import scala.io.Source
+import java.io.File
+
+
+object GridParser extends LazyLogging {
+  val exportedGridFile = s"${new File(".").getCanonicalPath}/src/main/python/pfGridExport/pfGrid.json"
+
+  def parse(gridFile: String = exportedGridFile): Option[PowerFactoryGrid] = {
+
+    val source =
+      Source.fromFile(gridFile)
+    val jsonString = try source.mkString
+    finally source.close
+
+    decode[PowerFactoryGrid](jsonString) match {
+      case Left(error) =>
+        logger.error(error.getMessage())
+        None
+      case Right(decodingResult) =>
+        Some(decodingResult)
+    }
+  }
+}
