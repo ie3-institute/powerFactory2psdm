be1e5c087d1de7fcfc5e46ab718323f87f8a1ee0
t-ober <63147366+t-ober@users.noreply.github.com>
2021-05-03 08:16:51 +0200

    add GridConverter

---
+++ src/main/scala/edu/ie3/powerFactory2psdm/converter/GridConverter.scala
@@ -0,0 +1,30 @@
+/*
+ * © 2021. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+ */
+
+package edu.ie3.powerFactory2psdm.converter
+
+import edu.ie3.powerFactory2psdm.exception.pf.MissingGridElementException
+import edu.ie3.powerFactory2psdm.model.powerfactory.PowerFactoryGrid
+import edu.ie3.powerFactory2psdm.model.powerfactory.PowerFactoryGrid.Nodes
+import org.jgrapht.alg.connectivity.BiconnectivityInspector
+import edu.ie3.powerFactory2psdm.model.powerfactory.PowerFactoryGridUtils
+
+case object GridConverter {
+
+  def convert(pfGrid: PowerFactoryGrid) = {
+    val gridElements = convertGridElements(pfGrid)
+  }
+
+  def convertGridElements(pfGrid: PowerFactoryGrid): Unit = {
+    val pfNodesMap: Map[String, Nodes] =
+      PowerFactoryGridUtils.getNodesMap(pfGrid)
+    val graph = GridGraphBuilder.build(pfGrid.nodes, pfGrid.lines, pfNodesMap)
+    val inspect = new BiconnectivityInspector(graph)
+    println("")
+
+  }
+
+}
