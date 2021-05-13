0ec388f89ea7772003702a68259d64b795e8f622
t-ober <63147366+t-ober@users.noreply.github.com>
2021-05-13 13:58:54 +0200

    add UUID2node map

--- src/main/scala/edu/ie3/powerFactory2psdm/model/powerfactory/PowerFactoryGridMaps.scala
+++ src/main/scala/edu/ie3/powerFactory2psdm/model/powerfactory/PowerFactoryGridMaps.scala
@@ -6,35 +6,46 @@
 
 package edu.ie3.powerFactory2psdm.model.powerfactory
 
-import edu.ie3.powerFactory2psdm.exception.pf.MissingGridElementException
+import edu.ie3.powerFactory2psdm.exception.pf.{
+  MissingGridElementException,
+  MissingParameterException
+}
 import edu.ie3.powerFactory2psdm.model.powerfactory.PowerFactoryGrid.{
   Lines,
   Nodes,
   Switches,
   Trafos2w
 }
-
 import java.util.UUID
 import com.typesafe.scalalogging.LazyLogging
-
 import scala.util.{Failure, Success, Try}
 
 class PowerFactoryGridMaps(pfGrid: PowerFactoryGrid) extends LazyLogging {
 
-  val uuid2node: Map[UUID, Nodes] = pfGrid.nodes match {
+  val UUID2node: Map[UUID, Nodes] = pfGrid.nodes match {
     case Some(nodes) => nodes.map(node => (UUID.randomUUID(), node)).toMap
     case None =>
       throw MissingGridElementException("There are no nodes in the Grid")
   }
 
-  val uuid2line: Map[UUID, Lines] = pfGrid.lines match {
+  val nodeId2UUID: Map[String, UUID] = UUID2node.map(
+    elem =>
+      (
+        elem._2.id.getOrElse(
+          throw MissingParameterException(s"Node $elem._2 has no id")
+        ),
+        elem._1
+      )
+  )
+
+  val UUID2line: Map[UUID, Lines] = pfGrid.lines match {
     case Some(lines) => lines.map(line => (UUID.randomUUID(), line)).toMap
     case None =>
       logger.debug("There are no lines in the grid")
       Map.empty
   }
 
-  val uuid2trafo2w: Map[UUID, Trafos2w] = pfGrid.trafos2w match {
+  val UUID2trafo2w: Map[UUID, Trafos2w] = pfGrid.trafos2w match {
     case Some(trafos2w) =>
       trafos2w.map(trafo2w => (UUID.randomUUID(), trafo2w)).toMap
     case None =>
@@ -42,27 +53,11 @@ class PowerFactoryGridMaps(pfGrid: PowerFactoryGrid) extends LazyLogging {
       Map.empty
   }
 
-  val uuid2switch: Map[UUID, Switches] = pfGrid.switches match {
+  val UUID2switch: Map[UUID, Switches] = pfGrid.switches match {
     case Some(switches) =>
       switches.map(switch => (UUID.randomUUID(), switch)).toMap
     case None =>
       logger.debug("There are no switches in the grid")
       Map.empty
-  }
-
-  def findNodeUuidFromLocName(locName: String): Try[UUID] = {
-    this.uuid2node
-      .find {
-        case (_, node) if node.loc_name.contains(locName) => true
-        case _                                            => false
-      }
-      .map { case (uuid, _) => Success(uuid) }
-      .getOrElse(
-        Failure(
-          new IllegalArgumentException(
-            s"The uuid to pf-node map does not contain a node with id $locName"
-          )
-        )
-      )
   }
 }
