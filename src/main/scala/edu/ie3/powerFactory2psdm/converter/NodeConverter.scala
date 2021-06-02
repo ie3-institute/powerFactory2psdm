d91072c044a601226d28215fc1175a0af7631264
t-ober <63147366+t-ober@users.noreply.github.com>
2021-06-02 09:19:20 +0200

    adjust code style

--- src/main/scala/edu/ie3/powerFactory2psdm/converter/NodeConverter.scala
+++ src/main/scala/edu/ie3/powerFactory2psdm/converter/NodeConverter.scala
@@ -58,9 +58,9 @@ object NodeConverter {
   /**
     * Converts a PowerFactory node into a PSDM node.
     *
-    * @param nodeUUID UUID of the PF node to convert
+    * @param nodeUUID  UUID of the PF node to convert
     * @param UUID2node Map of UUIDs and their associated [[Nodes]]
-    * @param subnet subnet the PF node lives in
+    * @param subnet    subnet the PF node lives in
     * @return a PSDM [[NodeInput]]
     */
   def convertNode(
@@ -69,7 +69,7 @@ object NodeConverter {
       subnet: Subnet
   ): NodeInput = {
     val pfNode = UUID2node(nodeUUID)
-    val id = UUID2node(nodeUUID).id.getOrElse("NO_ID")
+    val subnetNr = subnet.id
     val vTarget = pfNode.vtarget match {
       case Some(value) => Quantities.getQuantity(value, PU)
       case None        => Quantities.getQuantity(1d, PU)
@@ -77,25 +77,27 @@ object NodeConverter {
     val geoPosition: Point =
       CoordinateConverter.convert(pfNode.GPSlat, pfNode.GPSlon)
     val voltLvl = subnet.voltLvl
-    val subnetNr = subnet.id
-    val slack = isSlack(pfNode.conElms) match {
-      case Success(res) => res
-      case Failure(exc) =>
-        throw new IllegalArgumentException(
-          s"Exception occurred while checking if node with id ${pfNode.id
-            .getOrElse("NO_ID")} is a slack node. Exception: $exc"
+    (pfNode.id, isSlack(pfNode.conElms)) match {
+      case (Some(id), Success(isSlack)) =>
+        new NodeInput(
+          nodeUUID,
+          id,
+          OperatorInput.NO_OPERATOR_ASSIGNED,
+          OperationTime.notLimited(),
+          vTarget,
+          isSlack,
+          geoPosition,
+          voltLvl,
+          subnetNr
+        )
+      case (None, _) =>
+        throw ElementConfigurationException(s"The PF node $pfNode has no ID")
+      case (Some(id), Failure(exc)) =>
+        throw GridConfigurationException(
+          s"Exception occurred while checking if node with id $id is a slack node. Exception: $exc"
         )
     }
-    new NodeInput(
-      nodeUUID,
-      id,
-      OperatorInput.NO_OPERATOR_ASSIGNED,
-      OperationTime.notLimited(),
-      vTarget,
-      slack,
-      geoPosition,
-      voltLvl,
-      subnetNr
-    )
+
   }
+
 }
