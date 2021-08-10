85704e0eccd1bffa701e0b74d8b0dc5784c9979f
t-ober <63147366+t-ober@users.noreply.github.com>
2021-08-10 08:28:41 +0200

    use test config

--- src/main/scala/edu/ie3/powerFactory2psdm/main/RunConversion.scala
+++ src/main/scala/edu/ie3/powerFactory2psdm/main/RunConversion.scala
@@ -23,7 +23,7 @@ object RunConversion extends LazyLogging {
 
     logger.info("Parsing the config")
     val config =
-      ConfigSource.default.at("conversion-config").loadOrThrow[ConversionConfig]
+      ConfigSource.file("src/test/resources/application.conf").at("conversion-config").loadOrThrow[ConversionConfig]
     ConfigValidator.validate(config)
     val exportedGridFile =
       s"${new File(".").getCanonicalPath}/src/main/python/pfGridExport/pfGrid.json"
